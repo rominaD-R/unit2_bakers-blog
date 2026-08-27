888321b3472219b8ee372846c372c9f82de6ef5a
Romina Diaz-Rivero <romina.diazrivero@gmail.com>
2026-08-26 19:02:34 -0500

    Commiting spring boot stuff

---
+++ src/main/java/com/example/unit2_bakers_blog/Unit2BakersBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.unit2_bakers_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit2BakersBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit2BakersBlogApplication.class, args);
+	}
+
+}
